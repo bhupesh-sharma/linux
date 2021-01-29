d54842df9fe7e8b04525911acc3b4062c029447d
Thara Gopinath <thara.gopinath@linaro.org>
2021-01-28 21:37:09 -0500

    crypto: qce: skcipher: Improve the conditions for requesting AES fallback cipher
    The following are the conditions for requesting AES fallback cipher.
    	-  AES-192
    	- AES-XTS request with len <= 512 byte (Allow messages of length
    	  less than 512 bytes for all other AES encryption algorithms other
    	  than AES XTS)
    	- AES-XTS request with len > QCE_SECTOR_SIZE and is not a multiple
    	  of it
    
    Signed-off-by: Thara Gopinath <thara.gopinath@linaro.org>

--- drivers/crypto/qce/skcipher.c
+++ drivers/crypto/qce/skcipher.c
@@ -274,8 +274,13 @@ static int qce_skcipher_crypt(struct skcipher_request *req, int encrypt)
 		if (!IS_ALIGNED(req->cryptlen, blocksize))
 			return -EINVAL;
 
-	/* qce is hanging when AES-XTS request len > QCE_SECTOR_SIZE and
-	 * is not a multiple of it; pass such requests to the fallback
+	/*
+	 * Conditions for requesting a fallback cipher
+	 * AES-192 (not supported by crypto engine (CE))
+	 * AES-XTS request with len <= 512 byte (not recommended to use CE)
+	 * AES-XTS request with len > QCE_SECTOR_SIZE and
+	 * is not a multiple of it.(Revisit this condition to check if it is
+	 * needed in all versions of CE)
 	 */
 	if (IS_AES(rctx->flags) &&
 	    ((keylen != AES_KEYSIZE_128 && keylen != AES_KEYSIZE_256) ||
